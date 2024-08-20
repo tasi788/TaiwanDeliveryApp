4398c87e9a3f3d5d10fab0ae40843cbfd92d735c
tdc <tdc@tdccc.com.tw>
2024-08-20 17:50:31 +0800

    服務表

--- src/background.ts
+++ src/background.ts
@@ -4,8 +4,15 @@ import { Pchome24h } from './service/pchome24h.js';
 chrome.alarms.onAlarm.addListener(handleAlarm)
 
 
+class ServiceLoader{
+    pchome24h = Pchome24h;
+}
+
 async function handleAlarm(alarm: chrome.alarms.Alarm) {
-    console.log('Got alarm', alarm);
-    let client = new Pchome24h();
-    await client.load();
+    // console.log('Got alarm', alarm);
+    if (alarm.name === 'printLog') {
+        let client = new Pchome24h();
+        await client.load();
+        // await printLog();
+    }
 }
