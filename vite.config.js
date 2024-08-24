c55a2aebbc63d3bc8ce7cd24aca37e81f9594ee0
踢低吸 <tdc@sudo.host>
2024-08-24 16:26:17 +0800

    vitesjs 編譯設定

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,55 @@
 import { defineConfig } from 'vite'
+import path from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
+    base: './',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
     css: {
         preprocessorOptions: {
             sass: {
-                additionalData: `@import "@/index.sass";`, // Replace with your Sass file path
+                additionalData: `@import "@/index.sass";`,
             },
         },
     },
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, 'index.html'),
+                popup: path.resolve(__dirname, 'popup.html'),
+                background: path.resolve(__dirname, 'src/background.ts'),
+            },
+            output: {
+                entryFileNames: (chunkInfo) => {
+                    if (chunkInfo.name === 'background') {
+                        return 'background.js';
+                    }
+                    return '[name]-[hash].js';
+                }
+            }
+        },
+    },
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'manifest.json',
+                    dest: ''
+                },
+                {
+                    src: 'services.json',
+                    dest: ''
+                },
+                {
+                    src: 'images',
+                    dest: ''
+                },
+            ]
+        })
+    ]
 })
